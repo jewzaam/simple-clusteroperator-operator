be67b1520a34a832b10a23cffd50ea552317262f
Naveen Malik <nmalik@redhat.com>
2020-03-15 22:25:46 -0400

    WIP dynamic client test

--- go.mod
+++ go.mod
@@ -1,15 +1,25 @@
 module github.com/openshift/simple-clusteroperator-operator
 
 require (
-	github.com/openshift/api v0.0.0-20200311183032-85e16cc5dd7c // indirect
-	github.com/openshift/hive v0.0.0-20200310153125-9d6391083c54
-	github.com/operator-framework/operator-lifecycle-manager v0.0.0-20190128024246-5eb7ae5bdb7a
+	github.com/emicklei/go-restful v2.9.5+incompatible // indirect
+	github.com/go-openapi/jsonreference v0.19.3 // indirect
+	github.com/go-openapi/spec v0.19.3 // indirect
+	github.com/gogo/protobuf v1.2.2-0.20190723190241-65acae22fc9d // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/kubernetes/client-go v11.0.0+incompatible
+	github.com/mailru/easyjson v0.7.0 // indirect
 	github.com/operator-framework/operator-sdk v0.15.1-0.20200313185352-373ebf3751b6
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/tools v0.0.0-20200115044656-831fdb1e1868 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 	k8s.io/api v0.17.1
 	k8s.io/apimachinery v0.17.1
 	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/gengo v0.0.0-20190822140433-26a664648505 // indirect
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20191107075043-30be4d16710a // indirect
 	sigs.k8s.io/controller-runtime v0.1.12
 	sigs.k8s.io/controller-tools v0.1.10
 )
--- pkg/controller/simpleclusteroperator/simpleclusteroperator_controller_test.go
+++ pkg/controller/simpleclusteroperator/simpleclusteroperator_controller_test.go
@@ -1,7 +1,16 @@
 package simpleclusteroperator
 
 import (
+	"context"
+	"reflect"
 	"testing"
+
+	"github.com/kubernetes/client-go/dynamic/fake"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestNewResource(t *testing.T) {
@@ -23,5 +32,51 @@ func TestNewResource(t *testing.T) {
 	}
 	if resource.GetNamespace() != namespace {
 		t.Errorf("GetAPIVersion: expected '%s', found '%s'", namespace, resource.GetNamespace())
+	}
+}
+
+func newMockClient(t *testing.T, localObjects []runtime.Object) fake.FakeDynamicClient {
+	return fake.NewSimpleDynamicClient(runtime.NewScheme(), localObjects...)
+}
+
+func TestGetUnstructured(t *testing.T) {
+	// prepare the resources to feed to the mock client
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cm-name",
+			Namespace: "cm-namespace",
+		},
+		Data: map[string]string{
+			"KEY": "VALUE",
+		},
+	}
+
+	// create mock client w/ resources
+	client := newMockClient(t, []runtime.Object{
+		configMap,
+	})
+
+	resourceGkv := schema.GroupVersionResource{Group: "", Version: "v1", Resource: "configmaps"}
+
+	// try to find the config map
+	resource := newResource(
+		configMap.APIVersion,
+		configMap.Kind,
+		configMap.GetName(),
+		configMap.GetNamespace(),
+	)
+
+	err := client.Resource(resourceGkv).Get(context.TODO(), types.NamespacedName{Name: resource.GetName(), Namespace: resource.GetNamespace()}, resource)
+
+	if err != nil {
+		t.Errorf("Error getting resource: %s", err)
+	}
+
+	// the fleshed out `resource` should have Data now
+	if resource.Object["data"] == nil {
+		t.Errorf("Did not get back Data on ConfigMap.  type=%s", reflect.TypeOf(resource.Object))
+		for k := range resource.Object {
+			t.Errorf("key == %s", k)
+		}
 	}
 }
